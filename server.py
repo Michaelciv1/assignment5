19ff8d9aa7878a4eeb31f55644a6e546c0507481
Michael Wallerius <mikesterciv1@gmail.com>
2021-03-17 19:48:51 -0700

    deals with nonexistant directory

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import os
 HOST = "localhost"
 PORT = 5552
 
-print (sys.argv)
+print (sys.argv[0])
 
 with socket.socket() as s:
     s.bind((HOST, PORT))
@@ -19,12 +19,32 @@ with socket.socket() as s:
     while True:
         fromClient = conn.recv(1024).decode('utf-8')
 
-        if fromClient == "l":
+        if not fromClient:
+            break
+        print("This is",type(fromClient))
+
+        if fromClient[0] == "l":
             mesg = "Directories and files found under "+ currentDirectory +"\n"
             directoryList = os.listdir()
-            for file_folder in directoryList:
-                mesg += file_folder + "\n"
-            conn.send(str(mesg).encode('utf-8'))
+            if not directoryList:
+                conn.send("This directory is empty".encode('utf-8'))
+            else:
+                for file_folder in directoryList:
+                    mesg += file_folder + "\n"
+                conn.send(str(mesg).encode('utf-8'))
         
-        elif fromClient == "n":
+        elif fromClient[0] == "c":
+            try: 
+                os.chdir(currentDirectory+fromClient[1:])
+                currentDirectory = os.getcwd()
+                returnMessage = "Directory as been changed to " + currentDirectory
+                conn.send(returnMessage.encode('utf-8'))
+            except FileNotFoundError:
+                error = fromClient[1:] + " does not correspond to an existing directory"
+                conn.send(error.encode('utf-8'))
+
+        
+
             
+        
+        
